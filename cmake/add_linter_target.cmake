0cdb0b47e19daf86e982756663ba5b15d4a1f184
Jens Georg <jens.georg@desy.de>
2023-09-08 09:59:47 +0200

    feat: Make version of clang-tidy selectable

--- cmake/add_linter_target.cmake
+++ cmake/add_linter_target.cmake
@@ -1,9 +1,10 @@
-if (ENABLE_TIDY_WHILE_BUILDING)
-set(CMAKE_CXX_CLANG_TIDY /usr/bin/clang-tidy-14;-config-file=${CMAKE_SOURCE_DIR}/.clang-tidy)
-set(CMAKE_C_CLANG_TIDY /usr/bin/clang-tidy-14;-config-file=${CMAKE_SOURCE_DIR}/.clang-tidy)
+set(CTK_CLANG_TIDY_VERSION "14" CACHE STRING "Version of the clang-tidy binary to use")
+if (CTK_ENABLE_TIDY_WHILE_BUILDING)
+    set(CMAKE_CXX_CLANG_TIDY /usr/bin/clang-tidy-${CTK_CLANG_TIDY_VERSION};-config-file=${CMAKE_SOURCE_DIR}/.clang-tidy)
+    set(CMAKE_C_CLANG_TIDY /usr/bin/clang-tidy-${CTK_CLANG_TIDY_VERSION};-config-file=${CMAKE_SOURCE_DIR}/.clang-tidy)
 endif()
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-add_custom_target(run-linter run-clang-tidy-14 -header-filter ".*")
-add_custom_target(fix-linter run-clang-tidy-14 -header-filter ".*" -fix -format)
+add_custom_target(run-linter run-clang-tidy-${CTK_CLANG_TIDY_VERSION} -header-filter ".*")
+add_custom_target(fix-linter run-clang-tidy-${CTK_CLANG_TIDY_VERSION} -header-filter ".*" -fix -format)
 
 
